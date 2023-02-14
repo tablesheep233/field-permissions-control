8f6bef104802452f317da157b84f7d7932808cd5
tablesheep233 <858916094@qq.com>
2023-02-14 16:30:39 +0800

    init

---
+++ src/main/java/com/github/tablesheep233/permission/control/field/Main.java
@@ -0,0 +1,21 @@
+package com.github.tablesheep233.permission.control.field;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The type Main.
+ *
+ * @author <a href="mailto:858916094@qq.com">tablesheep233</a>
+ */
+@SpringBootApplication
+public class Main {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class);
+    }
+}
